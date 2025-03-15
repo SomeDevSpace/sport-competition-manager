1ded9c8448e2683b59269d2f77e0ce8ed23cd510
admin <admin@hadriel.net>
2025-03-15 21:50:18 +0100

    Enable authentication and add database setup with fixtures
    Introduced `compose.yaml` for PostgreSQL setup and added `UserFixtures` for user creation. Removed unused templates and adjusted navbar for proper logout handling.

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -5,13 +5,18 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
 
 final class HomeController extends AbstractController
 {
     #[Route('/', name: 'app_home')]
     public function index(): Response
     {
-        return $this->render('home/index.html.twig', [
-        ]);
+        if ($this->isGranted('IS_AUTHENTICATED')) {
+            return $this->redirectToRoute('app_dashboard');
+        }
+
+        return $this->redirectToRoute('app_login');
     }
 }
